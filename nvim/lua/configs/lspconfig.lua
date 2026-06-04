d14ee486d4b74b532c0e48d58f4191609c35cb1c
Porter Mecham <mecham.porter@gmail.com>
2026-06-04 12:04:25 -0600

    Added lots of lsp configs and mapping stuff. Also fixed the tmux.conf. I'm playing around with keeping common keymaps that already exist in the mappings.lua file as well

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -2,16 +2,18 @@ require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "pyright", "eslint_d", "eslint-lsp" }
 
+vim.lsp.enable(servers)
+
 -- read :h vim.lsp.config for changing options of lsp servers 
 --
-local lspconfig = require("lspconfig")
-local defaults = require("nvchad.configs.lspconfig")
-
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = defaults.on_attach,
-    on_init = defaults.on_init,
-    capabilities = defaults.capabilities,
-  }
-end
-
+-- local lspconfig = require("lspconfig")
+-- local defaults = require("nvchad.configs.lspconfig")
+--
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = defaults.on_attach,
+--     on_init = defaults.on_init,
+--     capabilities = defaults.capabilities,
+--   }
+-- end
+--
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -2,26 +2,47 @@ require "nvchad.mappings"
 
 -- add yours here
 
-local map = vim.keymap.set
+-- local M = {}
+--
+-- M.on_attach = function(client, bufnr)
+  local map = vim.keymap.set
 
-map("i", "jk", "<ESC>")
+  -- local opts = function(ldesc)
+  --   return { buffer = bufnr, silent = true, desc = ldesc }
+  -- end
 
-map("n", "<M-k>", "10k")
-map("n", "<M-j>", "10j")
-map("n", "<leader>sc", "<cmd>luafile $MYVIMRC<CR>", { desc = "Reload config"})
+  map("i", "jk", "<ESC>", { desc = "Exit insert mode"})
 
--- FZFLua
-map("n", "<leader>ff", "<cmd>FzfLua files<CR>", { desc = "Find files" })
-map("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", { desc = "Live grep" })
-map("n", "<leader>fb", "<cmd>FzfLua buffers<CR>", { desc = "Buffers" })
+  map("n", "<M-k>", "10k", { desc = "Move up 10 lines"})
+  map("n", "<M-j>", "10j", { desc = "Move down 10 lines"})
 
-map("t", "<C-h>", [[<C-\><C-n><C-w>h]], {desc = "Go to left window" })
-map("t", "<C-j>", [[<C-\><C-n><C-w>j]], {desc = "Go to down window" })
-map("t", "<C-k>", [[<C-\><C-n><C-w>k]], {desc = "Go to up window" })
-map("t", "<C-l>", [[<C-\><C-n><C-w>l]], {desc = "Go to right window" })
+  map("n", "<leader>rc", "<cmd>luafile $MYVIMRC<CR>", { desc = "[r]eload [c]onfig" })
 
--- Hop moving around file 
-map("n", ";;", "<cmd>HopCamelCase<CR>", {desc = "Hop Camel Case"})
+  map("n", "<leader>ff", "<cmd>FzfLua files<CR>", { desc = "[f]ind [f]iles" })
+  map("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", { desc = "[f]ind using [g]rep" })
+  map("n", "<leader>fb", "<cmd>FzfLua buffers<CR>", { desc = "[f]ind in [b]uffers" })
+  map("n", "<leader>rr","<cmd>%s/\r//<CR>", {desc = "[r]emove carriage [r]eturns"})
 
-map("n", "<leader>cp", "<cmd>:let @+ = expand('%')<CR>", {desc = "Copy path to clipboard"})
+  map("t", "<C-h>", [[<C-\><C-n><C-w>h]], { desc = "Go to left window" })
+  map("t", "<C-j>", [[<C-\><C-n><C-w>j]], { desc = "Go to down window" })
+  map("t", "<C-k>", [[<C-\><C-n><C-w>k]], { desc = "Go to up window" })
+  map("t", "<C-l>", [[<C-\><C-n><C-w>l]], { desc = "Go to right window" })
 
+  map("n", ";;", "<cmd>HopCamelCase<CR>", { desc = "Hop Camel Case" })
+
+  map("n", "<leader>cp", "<cmd>let @+ = expand('%')<CR>", { desc = "[C]opy [p]ath to clipboard" })
+  map("n", "<leader>od", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "[o]pen [d]iagnostics" })
+
+  -- Also use ]d to go to next diagnostic and [d to go to previous diagnostic. Plus lots of other navigation stuff with [ and ]
+
+  -- I was going to add a bunch of commands for lsp stuff but they're coverd already. :h vim.lsp
+  -- - "gra" (Normal and Visual mode) is mapped to |vim.lsp.buf.code_action()|
+  -- - "gri" is mapped to |vim.lsp.buf.implementation()|
+  -- - "grn" is mapped to |vim.lsp.buf.rename()|
+  -- - "grr" is mapped to |vim.lsp.buf.references()|
+  -- - "grt" is mapped to |vim.lsp.buf.type_definition()|
+  -- - "grx" is mapped to |vim.lsp.codelens.run()|
+  -- - "gO" is mapped to |vim.lsp.buf.document_symbol()|
+  -- - "K" get's the hover
+
+  -- Also use ]d to go to next diagnostic and [d to go to previous diagnostic. Plus lots of other navigation stuff with [ and ] :h [
