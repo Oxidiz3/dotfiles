6d68408f24b28c120650bd783ebf9c5e36d5944b
Porter Mecham <pmecham@Melaleuca.com>
2026-07-30 11:30:19 -0600

    setup a dotnet build and clean picker to build projects before we run them

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -25,8 +25,11 @@ return {
 
       dapui.setup()
 
-      local mason_bin = vim.fn.stdpath "data" .. "/mason/bin/"
-      local netcoredbg = mason_bin .. (vim.fn.has "win32" == 1 and "netcoredbg.cmd" or "netcoredbg")
+      local mason_root = vim.fn.stdpath "data" .. "/mason/"
+      local netcoredbg = mason_root .. "packages/netcoredbg/netcoredbg/netcoredbg.exe"
+      if vim.fn.has "win32" == 0 or vim.fn.filereadable(netcoredbg) == 0 then
+        netcoredbg = mason_root .. "bin/netcoredbg"
+      end
       if vim.fn.executable(netcoredbg) == 0 then
         netcoredbg = vim.fn.exepath "netcoredbg"
       end
@@ -64,20 +67,13 @@ return {
       dap.listeners.before.launch.dapui_config = function()
         dapui.open()
       end
-      dap.listeners.after.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.after.event_exited.dapui_config = function()
-        dapui.close()
-      end
-
       local map = vim.keymap.set
-      map("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-      map("n", "<F9>", dap.toggle_breakpoint, { desc = "Debug: Toggle breakpoint" })
+      map("n", "<leader>dc", dap.continue, { desc = "[d]ebug: Start/[c]ontinue" })
       map("n", "<F10>", dap.step_over, { desc = "Debug: Step over" })
       map("n", "<F11>", dap.step_into, { desc = "Debug: Step into" })
       map("n", "<S-F11>", dap.step_out, { desc = "Debug: Step out" })
       map("n", "<leader>du", dapui.toggle, { desc = "Debug: Toggle UI" })
+      map("n", "<leader>db", dap.toggle_breakpoint, { desc = "[d]ebug: Toggle [b]reakpoint" })
     end,
   },
 }
